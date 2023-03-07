0441cb44b6eeb21faaf7c099029668f7379a655a
samuel <shemlouk@gmail.com>
2023-03-07 13:34:05 -0300

    feat: add sign in page and session context to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SessionContext from "./hooks/SessionContext.js";
 import SignUp from "./pages/SignUp/index.js";
+import SignIn from "./pages/SignIn/index.js";
+import { useState } from "react";
 
 const App = () => {
+  const [session, setSession] = useState(null);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <SessionContext.Provider value={session}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn {...{ setSession }} />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </SessionContext.Provider>
   );
 };
 
