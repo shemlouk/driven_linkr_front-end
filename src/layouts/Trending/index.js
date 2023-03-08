3369861b4c17ac00ef4844afdb237774c798d88a
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-08 20:24:42 -0300

    feat: add trending sidebar with api data

---
+++ src/layouts/Trending/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import SessionContext from "../../hooks/SessionContext";
+import { HashtagList, HashtagName, TrendingSection } from "./styled";
+
+
+export default function Trending() {
+    const { session } = useContext(SessionContext);
+    const [isLoading, setIsLoading] = useState(true);
+    const [trendingTags, setTrandingTags] = useState(null);
+
+    useEffect(() => {
+        console.log(session);
+        getTrending();
+        console.log(session);
+    }, []);
+
+    async function getTrending() {
+        try {
+            const trending = (await axios.get(`${process.env.REACT_APP_API_URL}/trending`, session.auth)).data;
+            console.log(trending);
+            setTrandingTags(trending);
+            setIsLoading(false);
+        } catch (error) {
+            console.error(`getTrending ${error}`);
+            alert("An error occurred while trying to fetch the trending hashtags, please refresh the page.");
+        }
+    }
+
+    return (
+        <>
+            <TrendingSection>
+                <span>Trending</span>
+                <HashtagList>
+                {
+                    isLoading ? (<HashtagName>Carregando...</HashtagName>) : trendingTags.map((tag) => {
+                        return <HashtagName key={tag.id}># {tag.name}</HashtagName>
+                    })
+                }
+                </HashtagList>
+            </TrendingSection>
+        </>
+    );
+}
---
+++ src/layouts/Trending/styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+
+export const TrendingSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 300px;
+    max-height: 430px;
+    background-color: #171717;
+    margin-left: 18px;
+    border-radius: 12px;
+
+    span {
+        display: flex;
+        box-sizing: border-box;
+        padding-left: 16px;
+        align-items: center;
+        width: 100%;
+        height: 60px;
+        font-family: 'Oswald', sans-serif;
+        font-weight: 700;
+        font-size: 28px;
+        color: #FFFFFF;
+        border-bottom: 1px solid #484848;
+    }
+`;
+
+export const HashtagList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    margin-top: 24px;
+    height: 100%;
+    width: 100%
+`;
+
+export const HashtagName = styled.li`
+    font-size: 19px;
+    font-family: "Lato", sans-serif;
+    font-weight: 700;
+    box-sizing: border-box;
+    padding-left: 16px;
+    color: #FFFFFF;
+    margin-bottom: 16px;
+    cursor: pointer;
+
+    &:hover {
+        color: #c9c9c9;
+    }
+`;
