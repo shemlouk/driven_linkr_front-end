da113bb509e793d4286d72a5a1b45249279cbddc
samuel <shemlouk@gmail.com>
2023-03-07 22:32:51 -0300

    feat: add session validation for page refresh

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,5 +1,6 @@
 import { useCallback, useContext, useEffect, useState, useRef } from "react";
 import { IoSearchSharp, IoChevronDownSharp } from "react-icons/io5";
+import LoadingSpinner from "../../components/LoadingSpinner.js";
 import SessionContext from "../../hooks/SessionContext";
 import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../../utils/constants";
@@ -7,28 +8,58 @@ import * as S from "./style.js";
 import axios from "axios";
 
 const Header = () => {
+  const { setSession, session } = useContext(SessionContext);
+  const [sessionData, setSessionData] = useState(null);
   const [showLogOut, setShowLogOut] = useState(false);
-  const { user, auth } = useContext(SessionContext);
-  const loggedUser = useRef(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const profileRef = useRef(null);
   const navigate = useNavigate();
 
   const handleClick = useCallback((e) => {
-    if (e.target.parentElement === loggedUser.current) return;
+    if (e.target.parentElement === profileRef.current) return;
     setShowLogOut(false);
   });
 
   const logout = useCallback(async () => {
+    setIsLoading(true);
     try {
-      await axios.delete(`${API_URL}/signin`, auth);
+      await axios.delete(`${API_URL}/signin`, sessionData.auth);
       delete localStorage.session;
+      setSession(null);
       navigate("/");
     } catch ({ response }) {
+      setIsLoading(false);
       console.error(response);
       alert("Could not logout, please try again later!");
     }
   });
 
+  const validateSession = useCallback(async () => {
+    setIsLoading(true);
+    const localSession = JSON.parse(localStorage.getItem("session"));
+    if (!localSession) {
+      alert("Can't access this page.");
+      navigate("/");
+      return;
+    }
+    try {
+      await axios.get(`${API_URL}/signin/validate`, localSession.auth);
+      setSession({ session: localSession, setSession });
+      setSessionData({ ...localSession });
+      setIsLoading(false);
+    } catch ({ response }) {
+      alert("Session expired! Please sign in again.");
+      console.error(response);
+      navigate("/");
+    }
+  });
+
   useEffect(() => {
+    if (!session) {
+      validateSession();
+    } else {
+      setSessionData(session);
+    }
     window.addEventListener("click", handleClick);
     return () => {
       window.removeEventListener("click", handleClick);
@@ -36,26 +67,32 @@ const Header = () => {
   }, []);
 
   return (
-    <S.Header>
-      <S.LogoBox>
-        <Link to="/timeline">linkr</Link>
-      </S.LogoBox>
-      <S.SearchBar>
-        <input placeholder="Search for people" />
-        <div>
-          <IoSearchSharp />
-        </div>
-      </S.SearchBar>
-      <S.LoggedUser
-        ref={loggedUser}
-        active={showLogOut}
-        onClick={() => setShowLogOut(!showLogOut)}
-      >
-        <IoChevronDownSharp />
-        <img src={user.profilePicture} />
-        <div onClick={logout}>Logout</div>
-      </S.LoggedUser>
-    </S.Header>
+    <>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <S.Header>
+          <S.LogoBox>
+            <Link to="/timeline">linkr</Link>
+          </S.LogoBox>
+          <S.SearchBar>
+            <input placeholder="Search for people" />
+            <div>
+              <IoSearchSharp />
+            </div>
+          </S.SearchBar>
+          <S.LoggedUser
+            ref={profileRef}
+            active={showLogOut}
+            onClick={() => setShowLogOut(!showLogOut)}
+          >
+            <IoChevronDownSharp />
+            <img src={sessionData?.user?.profilePicture} />
+            <div onClick={logout}>Logout</div>
+          </S.LoggedUser>
+        </S.Header>
+      )}
+    </>
   );
 };
 
