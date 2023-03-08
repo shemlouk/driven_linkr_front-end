b275bedeb59197448b730ee49ea60349bd0fc495
DFCruzz <diegofcruzz@gmail.com>
2023-03-08 13:40:41 -0300

    refactor: finished timeline functions requisites

--- src/layouts/Timeline/index.js
+++ src/layouts/Timeline/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const Timeline = () => {
 
-
+    const [isLoading, setIsLoading] = useState(true)
     const [postList, setPostList] = useState([])
 
     useEffect(() => {
@@ -18,6 +18,7 @@ const Timeline = () => {
                 const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
                 console.log(res.data.slice(0, 20))
                 setPostList(res.data.slice(0, 20))
+                setIsLoading(false)
 
             } catch ({ response }) {
                 console.error(response)
@@ -38,37 +39,43 @@ const Timeline = () => {
                     <P.PostWrapper>
                         <WritePost />
                         <P.PostListing>
-                            {postList.map(post => (
-                                <P.PostBox key={post.id}>
-                                    <P.LeftSide>
-                                        <img src="https://www.w3schools.com/howto/img_avatar.png" />
-                                        <div>
-                                            <IoHeartOutline />
-                                            <p>13 likes</p>
-                                        </div>
-                                    </P.LeftSide>
-                                    <P.RightSide>
-                                        <P.PostUser>
-                                            <p>{post.userId}</p>
+                            {isLoading ? (
+                                <P.SpecialMessage>Loading...</P.SpecialMessage>
+                            ) : postList.length > 0 ? (
+                                postList.map((post) => (
+                                    <P.PostBox key={post.id}>
+                                        <P.LeftSide>
+                                            <img src="https://www.w3schools.com/howto/img_avatar.png" />
                                             <div>
-                                                <span><IoPencilSharp /></span>
-                                                <span><IoTrashOutline /></span>
+                                                <IoHeartOutline />
+                                                <p>13 likes</p>
                                             </div>
-                                        </P.PostUser>
-                                        <P.PostContent>
-                                            <p>{post.title}</p>
-                                            <P.LinkPreview>
+                                        </P.LeftSide>
+                                        <P.RightSide>
+                                            <P.PostUser>
+                                                <p>{post.userId}</p>
                                                 <div>
-                                                    <span>All Hope Is Gone (Audio)</span>
-                                                    <p>Slipknot's official audio stream for 'All Hope Is Gone' from the album, All Hope Is Gone - available now on Roadrunner Records. Download it at https://slipkn...</p>
-                                                    <p>https://www.youtube.com/watch?v=Wn2w3j_xmbw&ab_channel=Slipknot</p>
+                                                    <span><IoPencilSharp /></span>
+                                                    <span><IoTrashOutline /></span>
                                                 </div>
-                                                <img src="https://i.ytimg.com/vi/Wn2w3j_xmbw/maxresdefault.jpg" />
-                                            </P.LinkPreview>
-                                        </P.PostContent>
-                                    </P.RightSide>
-                                </P.PostBox>
-                            ))}
+                                            </P.PostUser>
+                                            <P.PostContent>
+                                                <p>{post.title}</p>
+                                                <P.LinkPreview>
+                                                    <div>
+                                                        <span>All Hope Is Gone (Audio)</span>
+                                                        <p>Slipknot's official audio stream for 'All Hope Is Gone' from the album, All Hope Is Gone - available now on Roadrunner Records. Download it at https://slipkn...</p>
+                                                        <p>https://www.youtube.com/watch?v=Wn2w3j_xmbw&ab_channel=Slipknot</p>
+                                                    </div>
+                                                    <img src="https://i.ytimg.com/vi/Wn2w3j_xmbw/maxresdefault.jpg" />
+                                                </P.LinkPreview>
+                                            </P.PostContent>
+                                        </P.RightSide>
+                                    </P.PostBox>
+                                ))
+                            ) : (
+                                <P.SpecialMessage>There are no posts yet.</P.SpecialMessage>
+                            )}
                         </P.PostListing>
                     </P.PostWrapper>
                     <P.SideMenu>
