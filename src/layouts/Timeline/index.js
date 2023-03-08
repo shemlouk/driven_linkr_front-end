52564f5f40698c041a40456741cf53f57bd1dc2a
DFCruzz <diegofcruzz@gmail.com>
2023-03-08 12:11:47 -0300

    feat: mock postList function added

--- src/layouts/Timeline/index.js
+++ src/layouts/Timeline/index.js
@@ -1,9 +1,32 @@
-import Header from "../Header/index"
-import * as P from "./styles"
+import Header from "../Header/index";
+import React, { useState, useEffect } from "react";
+import * as P from "./styles";
 import { IoHeartOutline, IoTrashOutline, IoPencilSharp } from "react-icons/io5";
 import WritePost from "../../components/WritePost";
+import axios from "axios";
 
 const Timeline = () => {
+
+
+    const [postList, setPostList] = useState([])
+
+    useEffect(() => {
+
+        async function getPosts() {
+
+            try {
+                const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+                console.log(res.data.slice(0, 20))
+                setPostList(res.data.slice(0, 20))
+
+            } catch ({ response }) {
+                console.error(response)
+                alert("An error occurred while trying to fetch the posts, please refresh the page.")
+            }
+        }
+        getPosts()
+    }, [])
+
     return (
         <>
             <Header />
@@ -15,68 +38,37 @@ const Timeline = () => {
                     <P.PostWrapper>
                         <WritePost />
                         <P.PostListing>
-                            <P.PostBox>
-                                <P.LeftSide>
-                                    <img src="https://www.w3schools.com/howto/img_avatar.png" />
-                                    <div>
-                                        <IoHeartOutline />
-                                        <p>13 likes</p>
-                                    </div>
-                                </P.LeftSide>
-                                <P.RightSide>
-                                    <P.PostUser>
-                                        <p>Lindo Nome</p>
+                            {postList.map(post => (
+                                <P.PostBox key={post.id}>
+                                    <P.LeftSide>
+                                        <img src="https://www.w3schools.com/howto/img_avatar.png" />
                                         <div>
-                                            <span><IoPencilSharp /></span>
-                                            <span><IoTrashOutline /></span>
+                                            <IoHeartOutline />
+                                            <p>13 likes</p>
                                         </div>
-                                    </P.PostUser>
-                                    <P.PostContent>
-                                        <p>
-                                            Muito Maneiro esse Tutorial de Material UI com React, deem uma olhada! <a>#react</a> <a>#material</a>
-                                        </p>
-                                        <P.LinkPreview>
+                                    </P.LeftSide>
+                                    <P.RightSide>
+                                        <P.PostUser>
+                                            <p>{post.userId}</p>
                                             <div>
-                                                <span>All Hope Is Gone (Audio)</span>
-                                                <p>Slipknot's official audio stream for 'All Hope Is Gone' from the album, All Hope Is Gone - available now on Roadrunner Records. Download it at https://slipkn...</p>
-                                                <p>https://www.youtube.com/watch?v=Wn2w3j_xmbw&ab_channel=Slipknot</p>
+                                                <span><IoPencilSharp /></span>
+                                                <span><IoTrashOutline /></span>
                                             </div>
-                                            <img src="https://i.ytimg.com/vi/Wn2w3j_xmbw/maxresdefault.jpg" />
-                                        </P.LinkPreview>
-                                    </P.PostContent>
-                                </P.RightSide>
-                            </P.PostBox>
-                            <P.PostBox>
-                                <P.LeftSide>
-                                    <img src="https://www.w3schools.com/howto/img_avatar.png" />
-                                    <div>
-                                        <IoHeartOutline />
-                                        <p>13 likes</p>
-                                    </div>
-                                </P.LeftSide>
-                                <P.RightSide>
-                                    <P.PostUser>
-                                        <p>Lindo Nome</p>
-                                        <div>
-                                            <span><IoPencilSharp /></span>
-                                            <span><IoTrashOutline /></span>
-                                        </div>
-                                    </P.PostUser>
-                                    <P.PostContent>
-                                        <p>
-                                            Muito Maneiro esse Tutorial de Material UI com React, deem uma olhada! <a>#react</a> <a>#material</a>
-                                        </p>
-                                        <P.LinkPreview>
-                                            <div>
-                                                <span>All Hope Is Gone (Audio)</span>
-                                                <p>Slipknot's official audio stream for 'All Hope Is Gone' from the album, All Hope Is Gone - available now on Roadrunner Records. Download it at https://slipkn...</p>
-                                                <p>https://www.youtube.com/watch?v=Wn2w3j_xmbw&ab_channel=Slipknot</p>
-                                            </div>
-                                            <img src="https://i.ytimg.com/vi/Wn2w3j_xmbw/maxresdefault.jpg" />
-                                        </P.LinkPreview>
-                                    </P.PostContent>
-                                </P.RightSide>
-                            </P.PostBox>
+                                        </P.PostUser>
+                                        <P.PostContent>
+                                            <p>{post.title}</p>
+                                            <P.LinkPreview>
+                                                <div>
+                                                    <span>All Hope Is Gone (Audio)</span>
+                                                    <p>Slipknot's official audio stream for 'All Hope Is Gone' from the album, All Hope Is Gone - available now on Roadrunner Records. Download it at https://slipkn...</p>
+                                                    <p>https://www.youtube.com/watch?v=Wn2w3j_xmbw&ab_channel=Slipknot</p>
+                                                </div>
+                                                <img src="https://i.ytimg.com/vi/Wn2w3j_xmbw/maxresdefault.jpg" />
+                                            </P.LinkPreview>
+                                        </P.PostContent>
+                                    </P.RightSide>
+                                </P.PostBox>
+                            ))}
                         </P.PostListing>
                     </P.PostWrapper>
                     <P.SideMenu>
