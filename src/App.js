d863a5ec20a576e23848a238d4b2c0256070aab3
samuel <shemlouk@gmail.com>
2023-03-09 15:13:47 -0300

    fix: remove session state and add session provider component to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import { SessionContextProvider } from "./hooks/SessionContext.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SessionContext from "./hooks/SessionContext.js";
 import Timeline from "./pages/Timeline/index.js";
 import SignUp from "./pages/SignUp/index.js";
 import SignIn from "./pages/SignIn/index.js";
-import { useState } from "react";
 
 const App = () => {
-  const [session, setSession] = useState(null);
   return (
-    <SessionContext.Provider value={{ session, setSession }}>
+    <SessionContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignIn {...{ setSession }} />} />
+          <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/timeline" element={<Timeline />} />
         </Routes>
       </BrowserRouter>
-    </SessionContext.Provider>
+    </SessionContextProvider>
   );
 };
 
