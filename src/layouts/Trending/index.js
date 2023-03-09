84d509b2edbbfcc220b9223c90e27d585cdedd16
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-09 09:43:07 -0300

    fix: add condition to load trending hashtags

--- src/layouts/Trending/index.js
+++ src/layouts/Trending/index.js
@@ -7,19 +7,16 @@ import { HashtagList, HashtagName, TrendingSection } from "./styled";
 export default function Trending() {
     const { session } = useContext(SessionContext);
     const [isLoading, setIsLoading] = useState(true);
-    const [trendingTags, setTrandingTags] = useState(null);
+    const [trendingTags, setTrendingTags] = useState(null);
 
     useEffect(() => {
-        console.log(session);
         getTrending();
-        console.log(session);
-    });
+    }, []);
 
     async function getTrending() {
         try {
             const trending = (await axios.get(`${process.env.REACT_APP_API_URL}/trending`, session.auth)).data;
-            console.log(trending);
-            setTrandingTags(trending);
+            setTrendingTags(trending);
             setIsLoading(false);
         } catch (error) {
             console.error(`getTrending ${error}`);
