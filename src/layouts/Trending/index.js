a3927b38b0ecd2db7ac805d26e34c4314d1f4b14
samuel <shemlouk@gmail.com>
2023-03-09 15:14:39 -0300

    fix: remove previous session context to new context

--- src/layouts/Trending/index.js
+++ src/layouts/Trending/index.js
@@ -1,44 +1,46 @@
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
-import SessionContext from "../../hooks/SessionContext";
 import { HashtagList, HashtagName, TrendingSection } from "./styled";
-
+import { SessionContext } from "../../hooks/SessionContext";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Trending() {
-    const { session } = useContext(SessionContext);
-    const [isLoading, setIsLoading] = useState(true);
-    const [trendingTags, setTrandingTags] = useState(null);
+  const [trendingTags, setTrandingTags] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { session } = useContext(SessionContext);
 
-    useEffect(() => {
-        console.log(session);
-        getTrending();
-        console.log(session);
-    });
+  useEffect(() => {
+    getTrending();
+  }, []);
 
-    async function getTrending() {
-        try {
-            const trending = (await axios.get(`${process.env.REACT_APP_API_URL}/trending`, session.auth)).data;
-            console.log(trending);
-            setTrandingTags(trending);
-            setIsLoading(false);
-        } catch (error) {
-            console.error(`getTrending ${error}`);
-            alert("An error occurred while trying to fetch the trending hashtags, please refresh the page.");
-        }
+  async function getTrending() {
+    try {
+      const trending = (
+        await axios.get(
+          `${process.env.REACT_APP_API_URL}/trending`,
+          session.auth
+        )
+      ).data;
+      setTrandingTags(trending);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(`getTrending ${error}`);
     }
+  }
 
-    return (
-        <>
-            <TrendingSection>
-                <span>Trending</span>
-                <HashtagList>
-                {
-                    isLoading ? (<HashtagName>Carregando...</HashtagName>) : trendingTags.map((tag) => {
-                        return <HashtagName key={tag.id}># {tag.name}</HashtagName>
-                    })
-                }
-                </HashtagList>
-            </TrendingSection>
-        </>
-    );
+  return (
+    <>
+      <TrendingSection>
+        <span>Trending</span>
+        <HashtagList>
+          {isLoading ? (
+            <HashtagName>Carregando...</HashtagName>
+          ) : (
+            trendingTags.map((tag) => {
+              return <HashtagName key={tag.id}># {tag.name}</HashtagName>;
+            })
+          )}
+        </HashtagList>
+      </TrendingSection>
+    </>
+  );
 }
