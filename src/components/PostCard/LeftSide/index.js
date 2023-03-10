d4a810ad9d1c9d2d976f68ecdc5f369ca4c487aa
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-10 19:00:17 -0300

    feat: show number of likes in a post

--- src/components/PostCard/LeftSide/index.js
+++ src/components/PostCard/LeftSide/index.js
@@ -4,13 +4,13 @@ import { useContext } from "react";
 import * as S from "./styles.js";
 
 const LeftSide = () => {
-  const { profilePicture, profile_picture, likes } = useContext(PostContext);
+  const { profilePicture, profile_picture, likes_count } = useContext(PostContext);
   return (
     <S.Container>
       <img src={profilePicture || profile_picture} />
       <div>
         <IoHeartOutline />
-        <p>{likes}</p>
+        <p>{`${likes_count || 0} likes`}</p>
       </div>
     </S.Container>
   );
