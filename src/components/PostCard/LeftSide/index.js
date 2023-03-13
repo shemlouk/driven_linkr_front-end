c41f9368fc5f745efea99385b534a6574895a55b
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-13 20:00:43 -0300

    feat: show the names of who liked the post and the quantity

--- src/components/PostCard/LeftSide/index.js
+++ src/components/PostCard/LeftSide/index.js
@@ -1,6 +1,6 @@
 import { SessionContext } from "../../../hooks/SessionContext.js";
 import { API_URL } from "../../../utils/constants/index.js";
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { IoHeartOutline, IoHeart } from "react-icons/io5";
 import PostContext from "../../../hooks/PostContext.js";
 import * as S from "./styles.js";
@@ -14,6 +14,7 @@ const LeftSide = () => {
     useContext(PostContext);
   const [isLoading, setIsLoading] = useState(false);
   const [numberLikes, setNumberLikes] = useState(Number(likes_count) || 0);
+  const [tooltipText, setTooltipText] = useState("");
   const { session } = useContext(SessionContext);
   const [isLiked, setIsLiked] = useState(likes_names?.includes(session.user.name));
 
@@ -22,6 +23,7 @@ const LeftSide = () => {
     setIsLiked(!isLiked);
     setNumberLikes(isLiked ? numberLikes - 1 : numberLikes + 1);
     setIsLoading(true);
+    loadTooltipText();
     try {
       await axios.post(`${API_URL}/timeline/${id}/like`, {}, session.auth);
       setIsLoading(false);
@@ -30,6 +32,32 @@ const LeftSide = () => {
     }
   }, [isLiked, isLoading]);
 
+  function loadTooltipText() {
+    const names = likes_names?.split(",");
+    let text = "";
+
+    switch (numberLikes) {
+      case 0:
+        setTooltipText("");
+        break;
+      case 1:
+        setTooltipText(isLiked? "Você" : likes_names);
+        break;
+      case 2:
+        isLiked ? text = `Você e ${names.find((n) => n !== session.user.name)}` : text = `${names[0]} e ${names[1]}`;
+        setTooltipText(text);
+        break;
+      default:
+        isLiked ? text = `Você, ${names.find((n) => n !== session.user.name)} e outras ${numberLikes - 2} pessoas` : text = `${names[0]}, ${names[1]} e outras ${numberLikes - 2} pessoas`;
+        setTooltipText(text);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    loadTooltipText();
+  }, [isLiked]);
+
   return (
     <S.Container {...{ isLiked }}>
       <img src={profilePicture || profile_picture} />
@@ -37,13 +65,13 @@ const LeftSide = () => {
         {isLiked ? (
           <IoHeart onClick={updateLike}
           data-tooltip-id="who-liked"
-          data-tooltip-content="Hello world"
+          data-tooltip-content={tooltipText}
           data-tooltip-place="bottom"
           />
         ) : (
           <IoHeartOutline onClick={updateLike}
           data-tooltip-id="who-liked"
-          data-tooltip-content="Hello world"
+          data-tooltip-content={tooltipText}
           data-tooltip-place="bottom"
           />
         )}
