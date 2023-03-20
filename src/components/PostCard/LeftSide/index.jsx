683d85f1efaeeedf50b2af4e332363697eb2e2eb
samuel <shemlouk@gmail.com>
2023-03-20 16:03:24 -0300

    fix: resolve conflicts

--- src/components/PostCard/LeftSide/index.jsx
+++ src/components/PostCard/LeftSide/index.jsx
@@ -1,21 +1,30 @@
+import { useCallback, useContext, useState, useEffect } from "react";
 import { SessionContext } from "../../../hooks/SessionContext";
-import { useCallback, useContext, useState } from "react";
 import { IoHeartOutline, IoHeart } from "react-icons/io5";
 import PostContext from "../../../hooks/PostContext";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
 import API from "../../../config/api";
+import "../../../assets/tooltip.css";
 import * as S from "./styles";
 
 const LeftSide = () => {
-  const { profilePicture, profile_picture, likes, id } =
+  const { profilePicture, profile_picture, likes_count, likes_names, id } =
     useContext(PostContext);
   const [isLoading, setIsLoading] = useState(false);
+  const [numberLikes, setNumberLikes] = useState(Number(likes_count) || 0);
+  const [tooltipText, setTooltipText] = useState("");
   const { session } = useContext(SessionContext);
-  const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(
+    likes_names?.includes(session.user.name)
+  );
 
   const updateLike = useCallback(async () => {
     if (isLoading) return;
     setIsLiked(!isLiked);
+    setNumberLikes(isLiked ? numberLikes - 1 : numberLikes + 1);
     setIsLoading(true);
+    loadTooltipText();
     try {
       await API.post(`/timeline/${id}/like`, {}, session.auth);
       setIsLoading(false);
@@ -24,16 +33,65 @@ const LeftSide = () => {
     }
   }, [isLiked, isLoading]);
 
+  function loadTooltipText() {
+    const names = likes_names?.split(",");
+    let text = "";
+
+    switch (numberLikes) {
+      case 0:
+        setTooltipText("");
+        break;
+      case 1:
+        setTooltipText(isLiked ? "Você" : likes_names);
+        break;
+      case 2:
+        isLiked
+          ? (text = `Você e ${names.find((n) => n !== session.user.name)}`)
+          : (text = `${names[0]} e ${names[1]}`);
+        setTooltipText(text);
+        break;
+      default:
+        isLiked
+          ? (text = `Você, ${names.find(
+              (n) => n !== session.user.name
+            )} e outras ${numberLikes - 2} pessoas`)
+          : (text = `${names[0]}, ${names[1]} e outras ${
+              numberLikes - 2
+            } pessoas`);
+        setTooltipText(text);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    loadTooltipText();
+  }, [isLiked]);
+
   return (
     <S.Container {...{ isLiked }}>
       <img src={profilePicture || profile_picture} />
       <div>
         {isLiked ? (
-          <IoHeart onClick={updateLike} />
+          <IoHeart
+            onClick={updateLike}
+            data-tooltip-id="who-liked"
+            data-tooltip-content={tooltipText}
+            data-tooltip-place="bottom"
+          />
         ) : (
-          <IoHeartOutline onClick={updateLike} />
+          <IoHeartOutline
+            onClick={updateLike}
+            data-tooltip-id="who-liked"
+            data-tooltip-content={tooltipText}
+            data-tooltip-place="bottom"
+          />
         )}
-        <p>{isLiked ? (likes ?? 0) + 1 : likes} likes</p>
+        <p>{numberLikes} likes</p>
+        <Tooltip
+          id="who-liked"
+          className="tooltip-two"
+          classNameArrow="tooltip-arrow"
+        />
       </div>
     </S.Container>
   );
