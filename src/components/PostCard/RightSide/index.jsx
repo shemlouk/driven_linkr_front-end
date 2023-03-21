1f76db8b4188c6f079f2b6da680c78db3d9efd09
DFCruzz <diegofcruzz@gmail.com>
2023-03-21 17:27:57 -0300

    feat: edit post functionts added, need review

--- src/components/PostCard/RightSide/index.jsx
+++ src/components/PostCard/RightSide/index.jsx
@@ -4,13 +4,44 @@ import PostContext from "../../../hooks/PostContext";
 import LinkPreview from "../../LinkPreview/index";
 import { ReactTagify } from "react-tagify";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import * as S from "./styles";
+import API from "../../../config/api";
 
 const RightSide = () => {
   const { id, user_id, name, description, openModal, selectHashtag } =
     useContext(PostContext);
   const { isLoggedIn, session } = useContext(SessionContext);
+  const [isEditing, setIsEditing] = useState(false)
+  const [descInput, setDescInput] = useState(description)
+  const [isLoading, setIsLoading] = useState(false)
+  const descRef = useRef(null)
+
+  function startEdit() {
+    setIsEditing(true)
+  }
+
+  function cancelEdit() {
+    setDescInput(description)
+    setIsEditing(false)
+  }
+
+  async function finishEdit(e) {
+    if (e.key === "Enter") {
+      setIsLoading(true)
+      try {
+        await API.put(`/user/post/${id}`, { description: descInput }, session.auth);
+        setIsEditing(false)
+      } catch (response) {
+        console.error(response);
+        alert("An error occurred while trying to edit your post");
+      }
+      setIsLoading(false)
+    }
+    if (e.key === "Escape") {
+      cancelEdit()
+    }
+  }
 
   return (
     <S.Container>
@@ -20,7 +51,7 @@ const RightSide = () => {
         </Link>
         {isLoggedIn && session.user.id === user_id ? (
           <div>
-            <span data-test="edit-btn">
+            <span data-test="edit-btn" onClick={() => isEditing ? cancelEdit() : startEdit()}>
               <IoPencilSharp />
             </span>
             <span data-test="delete-btn" onClick={() => openModal(id)}>
@@ -38,7 +69,19 @@ const RightSide = () => {
           }}
           tagClicked={(tag) => selectHashtag(tag)}
         >
-          <p data-test="description">{description}</p>
+          {isEditing ? (
+            <input
+              data-test="edit-input"
+              ref={descRef}
+              onKeyDown={finishEdit}
+              onChange={(e) => setDescInput(e.target.value)}
+              value={descInput}
+              type="text"
+              disabled={isLoading}
+            />
+          ) : (
+            <p data-test="description">{description}</p>
+          )}
         </ReactTagify>
         <LinkPreview />
       </S.PostContent>
