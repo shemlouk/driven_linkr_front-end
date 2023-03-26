407554fe5e60da470cd3e367274ff36b077e92ef
samuel <shemlouk@gmail.com>
2023-03-25 23:02:57 -0300

    feat: add modal context provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { SessionContextProvider } from "./hooks/SessionContext";
 import { PublishContextProvider } from "./hooks/PublishContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ModalContextProvider } from "./hooks/ModalContext";
 import HashtagContext from "./hooks/HashtagContext";
 import UserPage from "./pages/UserPage/index";
 import Timeline from "./pages/Timeline/index";
@@ -14,17 +15,23 @@ const App = () => {
   return (
     <SessionContextProvider>
       <PublishContextProvider>
-        <HashtagContext.Provider value={{ hashtag, setHashtag }}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<SignIn />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route exact path="/timeline" element={<Timeline />} />
-              <Route exact path="/hashtag/:hashtagName" element={<Hashtag />} />
-              <Route path="/user/:id?" element={<UserPage />} />
-            </Routes>
-          </BrowserRouter>
-        </HashtagContext.Provider>
+        <ModalContextProvider>
+          <HashtagContext.Provider value={{ hashtag, setHashtag }}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route exact path="/timeline" element={<Timeline />} />
+                <Route
+                  exact
+                  path="/hashtag/:hashtagName"
+                  element={<Hashtag />}
+                />
+                <Route path="/user/:id?" element={<UserPage />} />
+              </Routes>
+            </BrowserRouter>
+          </HashtagContext.Provider>
+        </ModalContextProvider>
       </PublishContextProvider>
     </SessionContextProvider>
   );
